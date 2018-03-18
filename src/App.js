4b2d19c125366c87ab1781014f05aaf0232e4524
Saurabh Modh <saurabhmodh94@gmail.com>
2018-03-18 15:34:51 +0530

    initial: Provider and createStore

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
+const store = createStore(() => { }, {}, applyMiddleware()); //very important line
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <PostForm />
-        <Posts />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <PostForm />
+          <Posts />
+        </div>
+      </Provider>
     );
   }
 }
