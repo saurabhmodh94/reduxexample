c55f5f7c31b977214b4895bfcc7c72fce9aee398
Saurabh Modh <saurabhmodh94@gmail.com>
2018-03-18 17:11:05 +0530

    PropTypes validation

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import { fetchPosts } from './../actions/postActions';
 class Posts extends Component {
   constructor(props) {
@@ -26,6 +27,10 @@ class Posts extends Component {
       </div>
     )
   }
+}
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
 }
 const mapStateToProps = state => ({
   posts: state.posts.items
